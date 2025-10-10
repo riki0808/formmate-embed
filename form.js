0e56f9fba417d8eb658956da07241ca44c51a199
riki0808 <sr28329732@gmail.com>
2025-10-10 17:34:18 +0900

    ログ出してみた

--- form.js
+++ form.js
@@ -1,15 +1,22 @@
 function setupIframe() {
   const iframe = document.getElementById('myIframe');
+  console.log('iframe',iframe)
   const allowedOrigins = ['http://localhost:3006', 'https://formmate.io', 'https://www.formmate.io'];
   window.addEventListener('message', function(event) {
+    console.log('event',event)
     if (!allowedOrigins.includes(event.origin)) return;
+    console.log('ここきた？')
     if (event.source !== iframe.contentWindow) return;
+    console.log('ここきた？2')
     const receivedData = event.data;
+    console.log('receivedData',receivedData)
     if (receivedData && receivedData.height && !isNaN(receivedData.height) && receivedData.height > 0) {
+      console.log('ここきた？3')
       iframe.style.height = receivedData.height + 'px';
     }
     if (receivedData?.type === 'redirect' && receivedData.url) {
       const redirectUrl = new URL(receivedData.url);
+      console.log('redirectUrl',redirectUrl)
       if(receivedData?.token){
         redirectUrl.searchParams.set('token', receivedData.token);
       }
